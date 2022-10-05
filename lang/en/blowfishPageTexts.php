70f7da19bac3023630e186930b762a03add18e22
Zdeněk Hejzlar <hejzlar@flexisoftware.cz>
2022-10-05 11:58:25 +0200

    Translate fix

--- lang/en/blowfishPageTexts.php
+++ lang/en/blowfishPageTexts.php
@@ -1,7 +1,10 @@
 <?php
 return [
     "annotation" => "Blowfish is a symmetric block cipher used in a large number of encryption products. Its variable key length of 32-448 bits makes this algorithm very secure.
-                    <br/>It is an algorithm created in response to the slower and older DES (Data Encryption Standard). It uses 64 bit blocks and 16 \"rounds\" of Feistel function in its encryption.
-                    <br/>The only drawback is the variable encryption key - each change slows down the process. There are 521 changes/computations of subkeys in the entire encryption cycle, which works out to about 4KB of data.",
+                    <br/><br/>It is an algorithm created in response to the slower and older DES (Data Encryption Standard). It uses 64 bit blocks and 16 \"rounds\" of Feistel function in its encryption.
+                    <br/><br/>The only drawback is the variable encryption key - each change slows down the process. There are 521 changes/computations of subkeys in the entire encryption cycle, which works out to about 4KB of data.
+                    <br/><br/>
+                    //TODO add more theoretical info
+                    ",
     "blockSchema" => "Block schema of blowfish",
 ];
