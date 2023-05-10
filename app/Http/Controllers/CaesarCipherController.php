16eb36b0990f7c910f1346f5c008df116f4e63d5
Kočičák <cheetumen@gmail.com>
2023-05-10 21:16:38 +0200

    Caesar own class

--- app/Http/Controllers/CaesarCipherController.php
+++ app/Http/Controllers/CaesarCipherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Algorithms\Caesar;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -23,13 +24,27 @@ class CaesarCipherController extends Controller
     {
         $timerStart = microtime(true);
         $data = $request->all();
-        $data["text"] = $this->normalize($data["text"]);
+
+        if(!is_numeric($data["shift"])){
+            Session::flash("alert-error",trans("baseTexts.keyCannotBeEmpty"));
+            return redirect("caesarCipher");
+        }
+        if(!is_string($data["text"]) || empty($data["text"])){
+            Session::flash("alert-error",trans("baseTexts.textCannotBeEmpty"));
+            return redirect("caesarCipher");
+        }
+        if(empty($data["action"])){
+            Session::flash("alert-error",trans("baseTexts.actionCannotBeEmpty"));
+            return redirect("caesarCipher");
+        }
+
+        $caesarCipher = new Caesar($data["text"]);
 
         if($data["action"] != "bruteforce"){
-            $data["finalText"] = $this->performCaesar($data["text"],$data["shift"],$data["action"]);
-            $data["shiftedAlphabet"] = $this->rotateAlphabet($request->input("shift"));
+            $data["finalText"] = $caesarCipher->performCaesar($data["shift"],$data["action"]);
+            $data["shiftedAlphabet"] = $caesarCipher->rotateAlphabet($request->input("shift"));
         }else{
-            $data["bruteForceResult"] = $this->bruteForce($data["text"]);
+            $data["bruteForceResult"] = $caesarCipher->bruteForce();
             $data["shift"] = 0;
         }
 
@@ -37,72 +52,5 @@ class CaesarCipherController extends Controller
         Session::flash("alert-info",trans("baseTexts.actionTook") . " ".$time_elapsed_secs . " s");
         Session::flash("data",$data);
         return redirect("caesarCipher");
-    }
-
-    function rotateAlphabet($key): array
-    {
-        $alphabet = [];
-        foreach (range("A","Z") as $char){
-            $alphabet[] = $char;
-        }
-        for ($i = 0; $i < $key; $i++) {
-             $temp = array_shift($alphabet);
-             $alphabet[] = $temp;
-        }
-        return $alphabet;
-    }
-
-    function bruteForce($textToDecrypt): array
-    {
-        $bruteForceResults = [];
-        for($i = 0; $i < 26; $i++){
-            $bruteForceResults[] = $this->performCaesar($textToDecrypt, $i, "decrypt");
-        }
-        return $bruteForceResults;
-    }
-
-    function normalize($string): string
-    {
-        $table = array(
-            'Š' => 'S', 'š' => 's', 'Đ' => 'Dj', 'đ' => 'dj', 'Ž' => 'Z', 'ž' => 'z', 'Č' => 'C', 'č' => 'c', 'Ć' => 'C', 'ć' => 'c',
-            'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'A', 'Ç' => 'C', 'È' => 'E', 'É' => 'E',
-            'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O',
-            'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ý' => 'Y', 'Þ' => 'B', 'ß' => 'Ss',
-            'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'a', 'ç' => 'c', 'è' => 'e', 'é' => 'e',
-            'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'o', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o',
-            'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ý' => 'y', 'ě' => 'e', 'þ' => 'b',
-            'ÿ' => 'y', 'Ŕ' => 'R', 'ŕ' => 'r',
-        );
-
-        return strtr($string, $table);
-    }
-
-    function performCaesar($text, $s,$type): string
-    {
-        $result = "";
-
-        if($type =="decrypt") $s = 26-$s;
-
-        // traverse text
-        for ($i = 0; $i < strlen($text); $i++)
-        {
-            if($text[$i] != " "){
-            // apply transformation to each
-            // character Encrypt Uppercase letters
-            if (ctype_upper($text[$i]))
-                $result = $result.chr((ord($text[$i]) +
-                            $s - 65) % 26 + 65);
-
-            // Encrypt Lowercase letters
-            else
-                $result = $result.chr((ord($text[$i]) +
-                            $s - 97) % 26 + 97);
-            }else{
-                $result = $result.chr(32);
-            }
-        }
-
-        // Return the resulting string
-        return $result;
     }
 }
