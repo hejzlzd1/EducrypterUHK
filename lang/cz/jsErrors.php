bdb821e33a525baf120c1ecb18fa7d146f853e3e
Zdeněk Hejzlar <hejzlzd1@uhk.cz>
2024-03-05 21:27:45 +0100

    fix: validation errors

--- lang/cz/jsErrors.php
+++ lang/cz/jsErrors.php
@@ -1,4 +1,6 @@
 <?php
 return [
     'inputCanBeOnlyBinary' => 'Vstup může obsahovat pouze 0 a 1',
+    'inputCanBeOnlyPrimeNumber' => 'Vstup může být pouze prvočíslo',
+    'inputCanBeOnlyPrimitiveRoot' => 'Vstup může být pouze primitivní kořen modula',
 ];
--- lang/en/jsErrors.php
+++ lang/en/jsErrors.php
@@ -1,4 +1,6 @@
 <?php
 return [
     'inputCanBeOnlyBinary' => 'Input can only contain 0 and 1',
+    'inputCanBeOnlyPrimeNumber' => 'Input can only be a prime number',
+    'inputCanBeOnlyPrimitiveRoot' => 'Input can only be a primitive root number',
 ];
