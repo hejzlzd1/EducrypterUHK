a6f2723ec1d2a16f26d4bf883d9c22582332fac5
Zdeněk Hejzlar <hejzlzd1@uhk.cz>
2024-03-18 17:51:13 +0100

    fix: minor text improvements before releasing

--- lang/cz/footerTexts.php
+++ lang/cz/footerTexts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'credits' => 'Tato webová stránka je projektem diplomové práce - Zdeněk Hejzlar',
+    'credits' => 'Tato webová stránka je <a href="https://github.com/hejzlzd1/EducrypterUHK">projektem</a> diplomové práce - Autor: <a href="https://www.linkedin.com/in/zdeněk-hejzlar-83a262241/">Zdeněk Hejzlar</a>',
     'usage' => 'Stránka je vhodná k výuce kryptografie na FIM',
 ];
--- lang/en/footerTexts.php
+++ lang/en/footerTexts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'credits' => 'This website is a project of the master thesis - Zdeněk Hejzlar',
+    'credits' => 'This website is a <a href="https://github.com/hejzlzd1/EducrypterUHK">project</a> of the master thesis - Author: <a href="https://www.linkedin.com/in/zdeněk-hejzlar-83a262241/">Zdeněk Hejzlar</a>',
     'usage' => 'The site is suitable for teaching cryptography at FIM',
 ];
